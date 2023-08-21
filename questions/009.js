f12bcb06d02d18eb58121165d65aa46cb24c84be
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-21 20:25:20 +0530

    1 file modified, 1 added

---
+++ questions/009.js
@@ -0,0 +1,37 @@
+// function calcsummation(a, b) {
+//   let i = +a + +b;
+//   return i;
+// }
+// let a = prompt('enter a value');
+// let b = prompt('enter b value');
+// let plus = calcsummation(a, b);
+// console.log(plus);
+
+//  function calcsubtraction(a, b) {
+//     let i = +a - +b;
+//     return i;
+//   }
+//   let a = prompt('enter a value');
+//   let b = prompt('enter b value');
+//   let plus = calcsubtraction(a, b);
+//   console.log(plus);
+
+  // function calcmultiplation(a, b) {
+  //     let i = +a * +b;
+  //     return i;
+  //   }
+  //   let a = prompt('enter a value');
+  //   let b = prompt('enter b value');
+  //   let plus = calcmultiplation(a, b);
+  //   console.log(plus);
+
+    //  function calcdivision(a, b) {1
+    //     let i = +a / +b;
+    //     return i;
+    //   }
+    //   let a = prompt('enter a value');
+    //   let b = prompt('enter b value');
+    //   let plus = calcdivision(a, b);
+    //   console.log(plus);
+      
+
