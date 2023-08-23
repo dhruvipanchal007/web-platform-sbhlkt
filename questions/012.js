6cf923f87507c7eeeb5c9362204e71c9ec557542
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-23 10:39:11 +0530

    2 files modified, 1 added

--- questions/012.js
+++ questions/012.js
@@ -1,5 +1,12 @@
-let  numbers=[10,5,50,150];
-let largenumber=numbers[0]
-for(let i=0,i<numbers.length,i++){
-  if(maxnumber>number[0])
+function findlargenumber(numbers) {
+  let maxnumber = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (maxnumber < numbers[i]) {
+      maxnumber = numbers[i];
+    }
+  }
+  return maxnumber;
 }
+let numbers = [5, 10, 25, 100];
+const largenumber = findlargenumber(numbers);
+console.log(largenumber);
