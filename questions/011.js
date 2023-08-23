a925446434927833d15d33802a436b396f8f2bf5
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-23 10:19:08 +0530

    1 file modified, 2 added

---
+++ questions/011.js
@@ -0,0 +1,23 @@
+// function daysoftheyear(days) {
+//   let i = days / 365;
+//   return i;
+// }
+// let days = prompt('enter days');
+// const years = daysoftheyear(days);
+// console.log(years);
+
+// function daysoftheMonth(days) {
+//   let i = days / 30.43;
+//   return i;
+// }
+// let days = prompt('enter days');
+// const Month = daysoftheMonth(days);
+// console.log(Month);
+
+function daysoftheyear(days) {
+  let i = days / 365;
+  return i;
+}
+let days = prompt('enter days');
+const years = daysoftheyear(days);
+console.log(years);
