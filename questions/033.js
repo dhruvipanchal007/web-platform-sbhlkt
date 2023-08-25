7ab9f09f1d308a2942f8f5128858a01e89ff92b8
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-25 11:35:26 +0530

    1 file modified, 2 added

---
+++ questions/033.js
@@ -0,0 +1,6 @@
+// let  i=prompt('enter sentence');
+let text = 'dhruvi panchal';
+let space = 'dhruvi dinesh panchal';
+// console.log(i.length);
+// console.log(text.split('\n').length);
+console.log(text.split(' ').length - 1);
