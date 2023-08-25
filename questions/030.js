70a3a7c427ddb956a26876934369ff49c382d564
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-25 11:14:02 +0530

    1 file modified, 4 added

---
+++ questions/030.js
@@ -0,0 +1,15 @@
+// const numbers = [5, 2, 8, 1, 3];
+
+// numbers.sort(function (a, b) {
+//   return a - b;
+// });
+
+// console.log( numbers);
+
+const numbers = [5, 2, 8, 1, 3];
+
+numbers.sort(function (a, b) {
+  return b - a;
+});
+
+console.log(numbers);
