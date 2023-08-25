9408cfc9341ecc8fd013f1f849cc131af0e31bcc
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-25 11:23:06 +0530

    1 file modified, 1 added, 1 deleted

---
+++ questions/032.js
@@ -0,0 +1,6 @@
+// let upperstring="HELLO DHRUVI";
+// const lowerstring=upperstring.toLowerCase();
+// console.log(lowerstring);
+let lower="hello dhruvi";
+const upper=lower.toUpperCase();
+console.log(upper);
