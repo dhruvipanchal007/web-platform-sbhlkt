8c4b52aaaddc511398c29d582cbe1b770d17572a
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-25 12:13:38 +0530

    1 file modified, 2 added

---
+++ questions/037.js
@@ -0,0 +1,11 @@
+function sum(a,b,c,d,e){
+  let i=a+b+c+d+e;
+  return i
+}
+let a=prompt('enter value');
+let b=prompt('enter value');
+let c=prompt('enter value');
+let d=prompt('enter value');
+let e=prompt('enter value');
+const M=sum(a,b,c,d,e);
+console.log(M); 
