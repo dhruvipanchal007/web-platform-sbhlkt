2b8afa104ba1663408d29fd567c3ad4138c78a45
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-01 11:32:27 +0530

    2 files modified

--- questions/017.js
+++ questions/017.js
@@ -1,17 +1,17 @@
 function daysnumber(number) {
-  if ((number = 1)) {
+  if ((number <=1)) {
     return 'sunday';
-  } else if ((number = 2)) {
+  } else if ((number <= 2)) {
     return 'Monday';
-  } else if ((number = 3)) {
+  } else if ((number <= 3)) {
     return 'Tuesday';
-  } else if ((number = 4)) {
+  } else if ((number <= 4)) {
     return 'wensday';
-  } else if ((number = 5)) {
+  } else if ((number <= 5)) {
     return 'thursday';
-  } else if ((number = 6)) {
+  } else if ((number <= 6)) {
     return 'friday';
-  } else if ((number = 7)) {
+  } else if ((number <= 7)) {
     return 'saturday';
   } else {
     return 'null';
