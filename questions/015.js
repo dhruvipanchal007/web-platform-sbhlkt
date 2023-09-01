5ac9d8fc8036bf46dbc9b4653e0041949df11b87
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-01 11:27:23 +0530

    3 files modified

---
+++ questions/015.js
@@ -0,0 +1,3 @@
+function findcharacter(c){
+  if()
+}
---
+++ questions/017.js
@@ -0,0 +1,22 @@
+function daysnumber(number) {
+  if ((number = 1)) {
+    return 'sunday';
+  } else if ((number = 2)) {
+    return 'Monday';
+  } else if ((number = 3)) {
+    return 'Tuesday';
+  } else if ((number = 4)) {
+    return 'wensday';
+  } else if ((number = 5)) {
+    return 'thursday';
+  } else if ((number = 6)) {
+    return 'friday';
+  } else if ((number = 7)) {
+    return 'saturday';
+  } else {
+    return 'null';
+  }
+}
+let number = prompt('enter number between 1 to 7');
+const Days = daysnumber(number);
+console.log(Days);
