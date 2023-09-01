e8463e513861e18d31dfd6bf0af03f19cb0be0f0
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-01 17:26:25 +0530

    1 file modified

--- questions/015.js
+++ questions/015.js
@@ -1,6 +1,16 @@
-let text = 'dhruvi';
-let code = text.charCodeAt(1);
-if(code>=65 && code<=90){
-  return capital;
+function capital(text) {
+  let code = text.charCodeAt(0);
+  if (code >= 65 && code <= 90) {
+    return "capital";
+  }
+  else if(code>=97 && code<=122) {
+    return "small";
+  }
+  else {
+    return "special"
+  }
 }
-console.log(code);
+let text=prompt('enter alphabate');
+const character=capital(text);
+console.log(character);
+
