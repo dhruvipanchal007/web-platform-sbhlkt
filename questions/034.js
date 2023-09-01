1d4f01087422bbda4905dffd1a5fe6a1fbfa96e3
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-01 18:32:20 +0530

    1 file modified

--- questions/034.js
+++ questions/034.js
@@ -7,3 +7,10 @@ const sortedString = sortString(inputString);
 
 console.log(sortedString);
 
+const x = "123";
+console.log(x.split('').reverse().join(''))
+
+// step 1 store no
+// step 2 create reverse of that no 
+// if no 1 and reverse no are equal then it's a palindrome
+// 321
