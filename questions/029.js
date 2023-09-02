aa9cc4b74048dd29b5dabdce18e6c666913a9e5a
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-02 18:39:31 +0530

    1 file modified

--- questions/029.js
+++ questions/029.js
@@ -2,34 +2,10 @@ function countDigits(number) {
   return String(number).length;
 }
 
-function isArmstrong(number) {
-  let num = number;
-  let power = countDigits(number);
-  let sum = 0;
+let digit1=prompt('enter 1 digitnumber');
+let digit2=prompt('enter 2 digitnumber');
+let digit3=prompt('enter 3 digitnumber');
 
-  while (num > 0) {
-      let digit = num % 10;
-      sum += Math.pow(digit, power);
-      num = Math.floor(num / 10);
-  }
+let  armstrongnumber=digit1*digit1*digit1 + digit2*digit2*digit2 + digit3*digit3*digit3;
+console.log(armstrongnumber);
 
-  return sum === number;
-}
-
-function findFirstNArmstrongNumbers(n) {
-  let armstrongNumbers = [];
-  let number = 1;
-
-  while (armstrongNumbers.length < n) {
-      if (isArmstrong(number)) {
-          armstrongNumbers.push(number);
-      }
-      number++;
-  }
-
-  return armstrongNumbers;
-}
-
-// Find and display the first 5 Armstrong numbers
-let first5ArmstrongNumbers = findFirstNArmstrongNumbers(5);
-console.log("The first 5 Armstrong numbers are:", first5ArmstrongNumbers.join(", "));
