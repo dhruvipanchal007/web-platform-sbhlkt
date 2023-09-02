f0ebaaf98403900c7f0e86ec2a9689d883d8e05b
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-02 12:08:45 +0530

    2 files modified

---
+++ questions/025.js
@@ -0,0 +1,20 @@
+let inputnumber=prompt('enter number');
+let num=Number(inputnumber);
+
+if(isNaN(num)){
+  console.log('invalid outtput')
+}
+else{
+  let reverseNum=0;
+  let originalNum=num;
+
+  while(num>0){
+    let digit=num %10;
+    reverseNum=reverseNum*10+digit;
+    num=Math.floor(num/10);
+  }
+  if(num===reverseNum){
+    console.log('plaindrome');
+  }
+}
+
