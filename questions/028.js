5882b9d0bf617e17ea49a88c6df51a6d59c13d79
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-02 17:54:00 +0530

    1 file modified

--- questions/028.js
+++ questions/028.js
@@ -1,9 +1,10 @@
 function decimaltohexadecimal(decimalnumber){
   let hexadecimal='';
+  let hexacharacter='123456789ABCDEF';
   while(decimalnumber>0){
     let reminder=decimalnumber % 16;
-    hexadecimal=reminder+hexadecimal;
-    decimalnumber=decimalnumber/16;
+    hexadecimal=hexacharacter[reminder]+hexadecimal;
+    decimalnumber=Math.floor(decimalnumber/16);
   }
   return hexadecimal;
 }
