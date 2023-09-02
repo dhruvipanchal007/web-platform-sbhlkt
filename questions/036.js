f0602e89d240985e54aaec9514fc42de5a79cb6f
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-02 18:13:57 +0530

    2 files modified

---
+++ questions/036.js
@@ -0,0 +1,14 @@
+let basis=200000;
+let Dapercentage=10;
+let Hrapercentage=7.50;
+let Ma=300;
+let Pfpercentage=12.50;
+
+let Da=(Dapercentage/100)*basis;
+let Hra=(Hrapercentage/100)*basis;
+let Pf=(Pfpercentage/100)*basis;
+
+let gross=basis+Da+Hra+Ma;
+let net=gross-Pf;
+console.log(net);
+
