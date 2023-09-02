55c3e76e1123c4817db978d189ca8d7fa19cb367
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-02 11:51:21 +0530

    1 file modified

--- questions/024.js
+++ questions/024.js
@@ -1,6 +1,20 @@
-let inputnumber=prompt('enter number');
-let num=Number(inputnumber);
+let inputnumber = prompt('enter number');
+let num = Number(inputnumber);
 
-if(isNaN(num)){
+if (isNaN(num)) {
   console.log('invalid output');
+} else {
+
+  let reverseNum = 0;
+  let originalNum = num;
+  
+  while(num>0){
+    let digit=num % 10;
+    reverseNum=reverseNum*10+digit;
+    num=Math.floor(num/10);
+  }
+  console.log(originalNum);
+  console.log(reverseNum);
+
 }
+
