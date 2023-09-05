b759b406a992b59551989b6efa79e37a793820f1
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-05 09:52:21 +0530

    1 file modified

--- questions/039.js
+++ questions/039.js
@@ -1,8 +1,18 @@
-for(let i=1;i<=5;i++){
-  let str="";
-  for(let j=1;j<=i;j++){
-    str +="*";
+// for(let i=1;i<=5;i++){
+//   let str="";
+//   for(let j=1;j<=i;j++){
+//     str +="*";
+//   }
+//   console.log(str);
+// }
+let num = 1;
+for (let i = 1; i <= 4; i++) {
+  let str = '';
+
+  for (let j = 1; j <= i; j++) {
+    if (j < i) {
+    }
+    num++;
   }
   console.log(str);
 }
-
