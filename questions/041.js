eb3e41e43e4e8749bebeb84fd1d5a9c812584c38
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-05 17:09:33 +0530

    1 file modified

--- questions/041.js
+++ questions/041.js
@@ -1,34 +1,57 @@
-let number=[10,20,30,40,50];
-let totalnumber=0;
-for(let i=0;i<number.length;i++){
-  totalnumber=totalnumber+number[i];
-}
+let number=[3,1,7,10,9,50]
+// let totalnumber=0;
+// for(let i=0;i<number.length;i++){
+//   totalnumber=totalnumber+number[i];
+// }
 
-let numberofinput=5;
-let average=totalnumber/numberofinput;
-console.log(totalnumber);
-console.log(average);
+// let numberofinput=5;
+// let average=totalnumber/numberofinput;
+// console.log(totalnumber);
+// console.log(average);
 
-function findmaxvalue(number){
-  let maxnumber=number[0];
-  for(let i=0;i<number.length;i++){
-    if(maxnumber<number[i]){
-      maxnumber=number[i];
+// function findmaxvalue(number){
+//   let maxnumber=number[0];
+//   for(let i=0;i<number.length;i++){
+//     if(maxnumber<number[i]){
+//       maxnumber=number[i];
+//     }
+//   }
+//   return maxnumber;
+// }
+// let largevalue=findmaxvalue(number);
+// console.log(largevalue);
+
+// function findminvalue(number){
+//   let smallnumber=number[0];
+//   for(let i=0;i<number.length;i++){
+//     if(smallnumber>number[i]){
+//       smallnumber=number[i];
+//     }
+//   }
+//   return smallnumber;
+// }
+// let smallvalue=findminvalue(number);
+// console.log(smallvalue);
+function findevennumber(numbers){
+  let even=0;
+  for(let i=0;i<=numbers.length;i++){
+    if (numbers[i] % 2 === 0) {
+     even++;
     }
   }
-  return maxnumber;
+  return even;
 }
-let largevalue=findmaxvalue(number);
-console.log(largevalue);
+let countnumber=findevennumber(number);
+console.log(countnumber);
 
-function findminvalue(number){
-  let smallnumber=number[0];
-  for(let i=0;i<number.length;i++){
-    if(smallnumber>number[i]){
-      smallnumber=number[i];
+function findoddnumber(numbers){
+  let odd=0;
+  for(let i=0;i<=numbers.length;i++){
+    if (numbers[i] % 2 !== 0) {
+     odd++;
     }
   }
-  return smallnumber;
+  return odd;
 }
-let smallvalue=findminvalue(number);
-console.log(smallvalue);
+let countoddnumber=findoddnumber(number);
+console.log(countoddnumber);
