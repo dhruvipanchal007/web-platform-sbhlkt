acc588da103e0f76e8d354912b2504b588228667
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-05 12:16:01 +0530

    2 files modified

---
+++ questions/041.js
@@ -0,0 +1,14 @@
+
+function calcsum(a,b,c,d,e){
+  let sum=(+a+ +b+ +c+ +d+ +e);
+  return sum;
+}
+
+let total=calcsum(a,b,c,d,e);
+console.log(total);
+
+let a=prompt('enter 1 no');
+let b=prompt('enter 2 no');
+let c=prompt('enter 3 no');
+let d=prompt('enter 4 no');
+let e=prompt('enter 5 no');
