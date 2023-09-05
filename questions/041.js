a383c648b917a263c719e86ba65d64b611ad0ef8
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-05 17:10:02 +0530

    1 file modified

--- questions/041.js
+++ questions/041.js
@@ -32,26 +32,26 @@ let number=[3,1,7,10,9,50]
 // }
 // let smallvalue=findminvalue(number);
 // console.log(smallvalue);
-function findevennumber(numbers){
-  let even=0;
-  for(let i=0;i<=numbers.length;i++){
-    if (numbers[i] % 2 === 0) {
-     even++;
-    }
-  }
-  return even;
-}
-let countnumber=findevennumber(number);
-console.log(countnumber);
+// function findevennumber(numbers){
+//   let even=0;
+//   for(let i=0;i<=numbers.length;i++){
+//     if (numbers[i] % 2 === 0) {
+//      even++;
+//     }
+//   }
+//   return even;
+// }
+// let countnumber=findevennumber(number);
+// console.log(countnumber);
 
-function findoddnumber(numbers){
-  let odd=0;
-  for(let i=0;i<=numbers.length;i++){
-    if (numbers[i] % 2 !== 0) {
-     odd++;
-    }
-  }
-  return odd;
-}
-let countoddnumber=findoddnumber(number);
-console.log(countoddnumber);
+// function findoddnumber(numbers){
+//   let odd=0;
+//   for(let i=0;i<=numbers.length;i++){
+//     if (numbers[i] % 2 !== 0) {
+//      odd++;
+//     }
+//   }
+//   return odd;
+// }
+// let countoddnumber=findoddnumber(number);
+// console.log(countoddnumber);
