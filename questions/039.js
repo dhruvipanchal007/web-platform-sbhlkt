507210e285ac2396f1321aed41bac6d6e1cf13f0
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-05 10:16:42 +0530

    1 file modified

--- questions/039.js
+++ questions/039.js
@@ -5,14 +5,29 @@
 //   }
 //   console.log(str);
 // }
-let num = 1;
-for (let i = 1; i <= 4; i++) {
-  let str = '';
+// let num = 1;
+// for (let i = 1; i <= 4; i++) {
+//   let str = '';
 
-  for (let j = 1; j <= i; j++) {
-    if (j < i) {
+//   for (let j = 1; j <= i; j++) {
+//     str+=num;
+//     num++;
+//     if(j<i){
+//       str+=',';
+//     }
+//   }
+//   console.log(str);
+// }
+
+for(let i=1;i<=5;i++){
+  let str="";
+  for(let j=1;j<=i;j++){
+    if((i+j)%2===0){
+      str+='1';
     }
-    num++;
+    else{
+      str+='0';
+    }
   }
   console.log(str);
 }
