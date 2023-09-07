1c4abb634f46d42e3ec85cdf30deb543d4235ac7
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-07 11:52:12 +0530

    2 files modified

--- questions/009.js
+++ questions/009.js
@@ -1,37 +1,58 @@
-// function calcsummation(a, b) {
-//   let i = +a + +b;
-//   return i;
-// }
-// let a = prompt('enter a value');
-// let b = prompt('enter b value');
-// let plus = calcsummation(a, b);
-// console.log(plus);
-
-//  function calcsubtraction(a, b) {
-//     let i = +a - +b;
-//     return i;
-//   }
-//   let a = prompt('enter a value');
-//   let b = prompt('enter b value');
-//   let plus = calcsubtraction(a, b);
-//   console.log(plus);
-
-  // function calcmultiplation(a, b) {
-  //     let i = +a * +b;
-  //     return i;
-  //   }
-  //   let a = prompt('enter a value');
-  //   let b = prompt('enter b value');
-  //   let plus = calcmultiplation(a, b);
-  //   console.log(plus);
-
-    //  function calcdivision(a, b) {1
-    //     let i = +a / +b;
-    //     return i;
-    //   }
-    //   let a = prompt('enter a value');
-    //   let b = prompt('enter b value');
-    //   let plus = calcdivision(a, b);
-    //   console.log(plus);
-      
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Home</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <!--
+      Need a visual blank slate?
+      Remove all code in `styles.css`!
+    -->
+    <link rel="stylesheet" href="styles.css" />
+    <!-- <script type="module" src="script.js"></script> -->
+    <!-- <script type="module" src="questions/001.js"></script> -->
+    <!-- <script type="module" src="questions/002.js"></script> -->
+    <!-- <script type="module" src="questions/003.js"></script> -->
+    <!-- <script type="module" src="questions/004.js"></script> -->
+    <!-- <script type="module" src="questions/005.js"></script> -->
+    <!-- <script type="module" src="questions/006.js"></script> -->
+    <!-- <script type="module" src="questions/007.js"></script> -->
+    <!-- <script type="module" src="questions/008.js"></script> -->
+    <!-- <script type="module" src="questions/009.js"></script> -->
+    <!-- <script type="module" src="questions/010.js"></script> -->
+    <!-- <script type="module" src="questions/011.js"></script> -->
+    <!-- <script type="module" src="questions/012.js"></script> -->
+    <!-- <script type="module" src="questions/013.js"></script> -->
+    <!-- <script type="module" src="questions/014.js"></script> -->
+    <!-- <script type="module" src="questions/015.js"></script> -->
+    <!-- <script type="module" src="questions/016.js"></script> -->
+    <!-- <script type="module" src="questions/017.js"></script> -->
+    <!-- <script type="module" src="questions/018.js"></script> -->
+    <!-- <script type="module" src="questions/019.js"></script> -->
+    <!-- <script type="module" src="questions/020.js"></script> -->
+    <!-- <script type="module" src="questions/021.js"></script> -->
+    <!-- <script type="module" src="questions/022.js"></script> -->
+    <!-- <script type="module" src="questions/023.js"></script> -->
+    <!-- <script type="module" src="questions/024.js"></script> -->
+    <!-- <script type="module" src="questions/025.js"></script> -->
+    <!-- <script type="module" src="questions/026.js"></script> -->
+    <!-- <script type="module" src="questions/027.js"></script> -->
+    <!-- <script type="module" src="questions/028.js"></script> -->
+    <!-- <script type="module" src="questions/029.js"></script> -->
+    <!-- <script type="module" src="questions/030.js"></script> -->
+    <!-- <script type="module" src="questions/031.js"></script> -->
+    <!-- <script type="module" src="questions/032.js"></script> -->
+    <!-- <script type="module" src="questions/033.js"></script> -->
+    <!-- <script type="module" src="questions/034.js"></script> -->
+    <!-- <script type="module" src="questions/035.js"></script> -->
+    <!-- <script type="module" src="questions/036.js"></script> -->
+    <!-- <script type="module" src="questions/037.js"></script> -->
+    <!-- <script type="module" src="questions/038.js"></script> -->
+    <!-- <script type="module" src="questions/039.js"></script> -->
+    <!-- <script type="module" src="questions/040.js"></script> -->
+    <!-- <script type="module" src="questions/041.js"></script> -->
+    <!-- <script type="module" src="questions/042.js"></script> -->
+    <!-- <script type="module" src="questions/043.js"></script> -->
+  </head>
+  <body></body>
+</html>
