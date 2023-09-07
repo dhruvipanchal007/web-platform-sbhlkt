fd5afbe2307d365a2f9b1f354d898da8cb7f2124
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-07 10:26:30 +0530

    2 files modified

--- questions/042.js
+++ questions/042.js
@@ -9,53 +9,53 @@
 // }
 // console.log(sum);
 // console.log(average);
-function displayMatrix(matrix) {
-  for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; j < matrix[i].length; j++) {
-          console.log(matrix[i][j] + ' ');
-      }
-      console.log('\n');
-  }
-}
+// function displayMatrix(matrix) {
+//   for (let i = 0; i < matrix.length; i++) {
+//       for (let j = 0; j < matrix[i].length; j++) {
+//           console.log(matrix[i][j] + ' ');
+//       }
+//       console.log('\n');
+//   }
+// }
 
-function multipyMatrices(matrixA, matrixB) {
-  const resultMatrix = [];
+// function multipyMatrices(matrixA, matrixB) {
+//   const resultMatrix = [];
 
-  for (let i = 0; i < matrixA.length; i++) {
-      const row = [];
+//   for (let i = 0; i < matrixA.length; i++) {
+//       const row = [];
 
-      for (let j = 0; j < matrixA[i].length; j++) {
-          row.push(matrixA[i][j] * matrixB[i][j]);
-      }
+//       for (let j = 0; j < matrixA[i].length; j++) {
+//           row.push(matrixA[i][j] * matrixB[i][j]);
+//       }
 
-      resultMatrix.push(row);
-  }
+//       resultMatrix.push(row);
+//   }
 
-  return resultMatrix;
-}
+//   return resultMatrix;
+// }
 
-const matrixA = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-];
+// const matrixA = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
 
-const matrixB = [
-  [9, 8, 7],
-  [6, 5, 4],
-  [3, 2, 1]
-];
+// const matrixB = [
+//   [9, 8, 7],
+//   [6, 5, 4],
+//   [3, 2, 1]
+// ];
 
-// Display Matrix A and Matrix B
-console.log("Matrix A:");
-displayMatrix(matrixA);
+// // Display Matrix A and Matrix B
+// console.log("Matrix A:");
+// displayMatrix(matrixA);
 
-console.log("Matrix B:");
-displayMatrix(matrixB);
+// console.log("Matrix B:");
+// displayMatrix(matrixB);
 
-// Add Matrix A and Matrix B
-const resultMatrix = multipyMatrices(matrixA, matrixB);
+// // Add Matrix A and Matrix B
+// const resultMatrix = multipyMatrices(matrixA, matrixB);
 
-// Display the result matrix
-console.log("Matrix A + Matrix B:");
-displayMatrix(resultMatrix);
+// // Display the result matrix
+// console.log("Matrix A + Matrix B:");
+// displayMatrix(resultMatrix);
